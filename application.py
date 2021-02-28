134bc34aed286cd279e335e350d358c8d208aa70
DebparnoB <69355442+DebparnoB@users.noreply.github.com>
2021-02-28 19:21:18 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,16 +1,18 @@
-from flask import Flask
-from rasa_nlu.model import Interpreter
-
-interpreter = Interpreter.load("models/nlu/default/banknlu")
-#result = interpreter.parse("Hey what'up?")
-
-application = Flask(__name__)
-
-@application.route('/')
-def hello_world():    
-    return 'Hello, World!'
-
-@application.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User '+ str(username)
+ @application.route('/')
+ def hello_world():    
+     return 'Hello, World!'
++    
++@application.route('/pred_intent/<user_text>')
++def show_user_profile(user_text):
++    # show the user profile for that user
++    result = interpreter.parse(user_text
++    return result["intent"]["name"]
+ 
+ @application.route('/user/<username>')
+ def show_user_profile(username):
+     # show the user profile for that user
+     return 'User '+ str(username)
++    
++if(__name__=='__main__'):
++    from waitress import serve
++    serve(application, host='0.0.0.0, port=8000)
